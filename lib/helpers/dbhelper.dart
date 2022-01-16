81c176942acd9f7b0dfba492074e79c49d27c805
coder_iota <akshat732000@gmail.com>
2022-01-17 00:41:36 +0530

    Added database connectivity and helper class.
    Added SQFLite dependency. Created DBHelper class to interact with the database.

---
+++ lib/helpers/dbhelper.dart
@@ -0,0 +1,51 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+class DBHelper {
+  static const tableName = "fav_stories";
+
+  static Future<sql.Database> _database() async {
+    final dbPath = await sql.getDatabasesPath();
+    try {
+      return sql.openDatabase(path.join(dbPath, "notes_app.db"),
+          onCreate: (db, version) {
+        try {
+          return db.execute(
+              'CREATE TABLE ${DBHelper.tableName}( id INTEGER PRIMARY KEY, descendants INTEGER, score INTEGER, by TEXT, title TEXT, url TEXT, time TEXT)');
+        } catch (err) {
+          throw Exception("Error Creating DB");
+        }
+      }, version: 1);
+    } catch (err) {
+      throw Exception("ERROR Opening DB");
+    }
+  }
+
+  static Future<List<Map<String, Object?>>> getAllFavorites() async {
+    final db = await _database();
+    return db.query(tableName);
+  }
+
+  static Future<List<Map<String, Object?>>> getFavoriteById(int storyId) async {
+    final db = await _database();
+    return db.query(tableName, where: "id = ?", whereArgs: [storyId]);
+  }
+
+  static Future<int> addFavorite(Map<String, Object> vals) async {
+    final db = await _database();
+    try {
+      return db.insert(tableName, vals);
+    } catch (err) {
+      throw Exception("Error adding Favorite");
+    }
+  }
+
+  static Future<int> removeFromFavorite(int storyId) async{
+    final db = await _database();
+    try{
+    return db.delete(tableName, where: "id = ?", whereArgs: [storyId]);
+    } catch (err){
+      throw Exception("Error deleting from fav.");
+    }
+  }
+}
